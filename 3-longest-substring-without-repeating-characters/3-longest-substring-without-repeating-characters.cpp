7583f9050050b454a2e0c120d58828be36833144
Ashvin Arora <95213058+AshvinArora07@users.noreply.github.com>
2022-09-05 11:24:30 +0530

    Time: 37 ms (40.85%), Space: 8.5 MB (49.32%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -7,7 +7,7 @@ public:
         map<char,int> m;
         for(end=0;end<s.length();end++){
             newchar=s[end];
-            if(m.find(s[end])!=m.end())
+            if(m.find(newchar)!=m.end())
                 start = max(start,m.find(newchar)->second+1);
             m[newchar]=end;
             length=max(length,end-start+1);
