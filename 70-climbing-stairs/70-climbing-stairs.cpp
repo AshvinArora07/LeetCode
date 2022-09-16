d3e379b89ad6879efcbd9ca23a312de3b34b7513
Ashvin Arora <95213058+AshvinArora07@users.noreply.github.com>
2022-09-16 11:53:16 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.40%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int climbStairs(int n) {
@@ -6,11 +7,32 @@ public:
             return 1;
         if(n==1)
             return 1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=n;i++){                                                             // Iterative Approach
             temp=two;
             two=one+two;
             one=temp;
         }
         return two;
+    }
+};
+
+// Recursion
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<2)                                                                                        //Recursive Approach      
+            return 1;
+        return  climbStairs(n-1)+climbStairs(n-2); // from nth stair to the 0th stair                                                     
+    }
+};
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[n+1];
+        dp[1]=1;
+        if(n>=2) dp[2]=2;                                                                                //DP approaach
+        for(int i=3;i<=n;i++)  dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
