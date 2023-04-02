3c998af9237c3a0e3824ca8eb1b922dae74b1a49
Ashvin Arora <95213058+AshvinArora07@users.noreply.github.com>
2023-04-03 03:04:46 +0530

    Time: 352 ms (7.42%), Space: 118.8 MB (5.17%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {   
+        unordered_map<int, pair<int,int>> m; //{index,numpair}
+        //Now creating a copy of spells vector
+        vector<int> numspair(spells);
+        //Now storing the spells and mapping its index onto the map
+        for(int i=0;i<spells.size();i++){
+            m[spells[i]]={i,-1};
+        }
+        //Now sorting both the vectors
+        //Considering the length m of spells
+        sort(spells.begin(),spells.end()); // O(m log m)
+        //Considering the length n of potions
+        sort(potions.begin(),potions.end());// O(n log n)
+        
+        int potionptr=potions.size()-1;
+        
+        // Two Pointer Approach
+        //Starting spell pointer from left
+        // And potion pointer from right
+        for(int i=0;i<spells.size();i++){
+            int spell=spells[i];
+            while(potionptr>=0 && (long long) spell*potions[potionptr]>=success){
+                potionptr--;
+            }
+            m[spell]={m[spell].first,potions.size()-1-potionptr};
+           /*cout<<"Value:"<<spell<<"-->"<<"Index:"<<m[spell].first<<"-->"<<"count:"<<m[spell].second<<endl;------>this shows how count is being stored for every index and the index's value*/
+        }
+        int index=0;
+        for(int spell: numspair){
+            numspair[index++]=m[spell].second;
+        }
+        return numspair;
+    }
+};
