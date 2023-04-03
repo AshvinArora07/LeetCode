9fb0e0f62fcd8d2c6071ac70835e1d95a7efe9fb
Ashvin Arora <95213058+AshvinArora07@users.noreply.github.com>
2023-04-04 00:57:10 +0530

    Time: 103 ms (9.80%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        //It can be done with TWO  POINTER
-        //sorting the people vector
-        //if the lightest and the heaviest person can go on the same boat i,e.people[i]+people[j]
-        //people[i]+people[j]<=limit incrementing the count of boat.
-        
-        /*IMPLEMENTATION*/
         int i=0,j=people.size()-1,count=0;
         sort(people.begin(),people.end());
         while(i<=j){
             if(people[i]+people[j]<=limit)
                 i++;
             count++;
-            j--;// Once we know the lighest and heaviest can travel on one boat we have to decrement the right pointer(j) if people[i]+people[j]>limnit.
+            j--;
         }
         return count;
     }
