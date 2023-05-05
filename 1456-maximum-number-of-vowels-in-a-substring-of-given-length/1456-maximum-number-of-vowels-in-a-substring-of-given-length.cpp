ba743f8d0bd9e435ab6048ca5e136470f4199cc0
Ashvin Arora <95213058+AshvinArora07@users.noreply.github.com>
2023-05-05 07:51:22 +0530

    Time: 7 ms (99.91%), Space: 10 MB (68.38%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,13 +1,15 @@
 class Solution {
-public:
-    
+public:  
     bool isvowel(char c){
-        return c=='a' or c=='e' or c=='i' or c=='o' or c=='u';
+        if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u')
+            return 1;
+        return 0;
     }
     
     int maxVowels(string s, int k) {
-        int res=0,len=0,i=0,j=0,ctv=0;
         int n=s.size();
+        /* 1 Solution
+        int res=0,len=0,i=0,j=0,ctv=0;
         while(j<n){
             if(isvowel(s[j]))
                 ctv++;
@@ -21,5 +23,16 @@ public:
             j++;
         }
         return res;
+        */
+        // Sliding Window Approach
+        int curr_vow=0, max_vow=0;
+        for(int i=0;i<n;i++){
+            curr_vow+=isvowel(s[i]);
+            if(i>=k)
+                curr_vow-=isvowel(s[i-k]);
+            //because when we shift to the next window after length k we delete the vowel at i-k position which will not be included int the current window after moving from previous window
+            max_vow=max(max_vow,curr_vow);
+        }
+        return max_vow;
     }
 };
